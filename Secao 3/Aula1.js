dc97d3e4c5f7186738541f9c9c5a785830eb80c7
Matheus Paiva Loures <mploures@ufmg.br>
2021-10-06 23:40:38 -0300

    Dia 1 : Basico

---
+++ Secao 3/Aula1.js
@@ -0,0 +1,21 @@
+console.log("Teste 1"); // exemplo de Sentença de codigo
+
+console.log(
+    "Teste 2"
+    )// exemplo de Sentença de codigo
+
+
+//------------------------------------------------------//
+
+console.log("1+1 "+(1+1)); // imprime  um texto no terminal
+
+//------------------------------------------------------//
+{
+    {
+        {
+           //Bloco de codigo 
+        }
+    }
+}
+
+
